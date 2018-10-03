408b1fc99f37fef1ac16a582460db670cfd6accb
zamroni <zamroni@dominopos.com>
2018-10-03 14:55:02 +0800

    create program to output GUID

---
+++ guid.pas
@@ -0,0 +1,21 @@
+{------------------------------------------------
+ Small utility program to output GUID
+
+ This is provided mostly to enable generate GUID
+ for Pascal interface identifier
+ on shell so we can use in text editor that does not
+ provide such tool.
+
+ @author Zamrony P. Juhara <zamronypj@yahoo.com>
+-----------------------------------------------}
+
+program guid;
+
+uses sysutils;
+
+var aguid:TGUID;
+
+begin
+    createGUID(aguid);
+    write(GUIDToString(aguid));
+end.
